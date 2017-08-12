440c1d33df3362f7e681d5c6d07b4dd3b724e04e
Raunak Bhansali <raunak@webklipper.com>
2017-08-12 20:06:38 +0530

    Added project clj file. For project meta data.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject trie-clj "0.1.0"
+  :description "Trie Data Structures implementation"
+  :url "https://github.com/raunakb94/trie-clj"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+
+  :profiles {:uberjar {:aot :all}})
